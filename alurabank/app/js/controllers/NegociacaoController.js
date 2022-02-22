fd3d9446f72f88c6a1b4ca031766b0d7c9584cfe
Hadnan Basilio | BomTrato <hbasilio@bomtrato.com>
2022-02-22 16:56:32 -0300

    Seguindo com decorators e API

--- alurabank/app/js/controllers/NegociacaoController.js
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -1,7 +1,13 @@
-System.register(["../models/index", "../views/index"], function (exports_1, context_1) {
+System.register(["../helpers/decorators/index", "../models/index", "../views/index"], function (exports_1, context_1) {
     "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
     var __moduleName = context_1 && context_1.id;
-    var index_1, index_2, NegociacaoController;
+    var index_1, index_2, index_3, NegociacaoController, DiaDaSemana;
     return {
         setters: [
             function (index_1_1) {
@@ -9,28 +15,72 @@ System.register(["../models/index", "../views/index"], function (exports_1, cont
             },
             function (index_2_1) {
                 index_2 = index_2_1;
+            },
+            function (index_3_1) {
+                index_3 = index_3_1;
             }
         ],
         execute: function () {
             NegociacaoController = class NegociacaoController {
                 constructor() {
-                    this._negociacoes = new index_1.Negociacoes();
-                    this._negociacoesView = new index_2.NegociacoesView("#negociacoesView");
-                    this._mensagemView = new index_2.MensagemView("#mensagemView");
-                    this._inputData = $("#data");
-                    this._inputQuantidade = $("#quantidade");
-                    this._inputValor = $("#valor");
+                    this._negociacoes = new index_2.Negociacoes();
+                    this._negociacoesView = new index_3.NegociacoesView("#negociacoesView");
+                    this._mensagemView = new index_3.MensagemView("#mensagemView");
                     this._negociacoesView.update(this._negociacoes);
                 }
                 adiciona(event) {
                     event.preventDefault();
-                    const negociacao = new index_1.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    let data = new Date(this._inputData.val().replace(/-/g, ','));
+                    if (!this._ehDiaUtil(data)) {
+                        this._mensagemView.update("Somente negociações em dias úteis");
+                        return;
+                    }
+                    const negociacao = new index_2.Negociacao(data, parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
                     this._negociacoes.adiciona(negociacao);
                     this._negociacoesView.update(this._negociacoes);
                     this._mensagemView.update("Negociação adicionada com sucesso!");
                 }
+                _ehDiaUtil(data) {
+                    return data.getDay() != DiaDaSemana.Domingo && data.getDay() != DiaDaSemana.Sabado;
+                }
+                importaDados() {
+                    function isOk(res) {
+                        if (!res.ok) {
+                            throw new Error(res.statusText);
+                        }
+                        return res;
+                    }
+                    fetch('http://localhost:8080/dados')
+                        .then(res => isOk(res))
+                        .then(res => res.json())
+                        .then((dados) => {
+                        dados
+                            .map(dado => new index_2.Negociacao(new Date(), dado.vezes, dado.montante))
+                            .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+                        this._negociacoesView.update(this._negociacoes);
+                    })
+                        .catch(err => console.log(err.message));
+                }
             };
+            __decorate([
+                index_1.domInject('#data')
+            ], NegociacaoController.prototype, "_inputData", void 0);
+            __decorate([
+                index_1.domInject('#quantidade')
+            ], NegociacaoController.prototype, "_inputQuantidade", void 0);
+            __decorate([
+                index_1.domInject('#valor')
+            ], NegociacaoController.prototype, "_inputValor", void 0);
             exports_1("NegociacaoController", NegociacaoController);
+            (function (DiaDaSemana) {
+                DiaDaSemana[DiaDaSemana["Domingo"] = 0] = "Domingo";
+                DiaDaSemana[DiaDaSemana["Segunda"] = 1] = "Segunda";
+                DiaDaSemana[DiaDaSemana["Terca"] = 2] = "Terca";
+                DiaDaSemana[DiaDaSemana["Quarta"] = 3] = "Quarta";
+                DiaDaSemana[DiaDaSemana["Quinta"] = 4] = "Quinta";
+                DiaDaSemana[DiaDaSemana["Sexta"] = 5] = "Sexta";
+                DiaDaSemana[DiaDaSemana["Sabado"] = 6] = "Sabado";
+            })(DiaDaSemana || (DiaDaSemana = {}));
         }
     };
 });
--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,26 +1,36 @@
-import { Negociacao, Negociacoes } from "../models/index";
+import { domInject } from "../helpers/decorators/index";
+import { Negociacao, NegociacaoParcial, Negociacoes } from "../models/index";
 import { MensagemView, NegociacoesView } from "../views/index";
 
 export class NegociacaoController {
+    @domInject('#data')
     private _inputData: JQuery;
+    
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+
+    @domInject('#valor')
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView("#negociacoesView");
     private _mensagemView = new MensagemView("#mensagemView");
 
     constructor() {
-        this._inputData = $("#data")
-        this._inputQuantidade = $("#quantidade")
-        this._inputValor = $("#valor")
         this._negociacoesView.update(this._negociacoes)
     }   
 
     adiciona(event: Event) {
         event.preventDefault()
 
+        let data: Date = new Date(this._inputData.val().replace(/-/g, ','))
+
+        if(!this._ehDiaUtil(data)) {
+            this._mensagemView.update("Somente negociações em dias úteis");
+            return
+        }
+
         const negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g, ',')),
+            data,
             parseInt(this._inputQuantidade.val()),
             parseFloat(this._inputValor.val())
         )
@@ -29,4 +39,38 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes)
         this._mensagemView.update("Negociação adicionada com sucesso!")
     }
+
+    private _ehDiaUtil(data: Date): boolean {
+        return data.getDay() != DiaDaSemana.Domingo && data.getDay() != DiaDaSemana.Sabado
+    }
+
+    importaDados() {
+
+        function isOk(res: Response) {
+            if(!res.ok) {
+                throw new Error(res.statusText)
+            }
+            return res
+        }
+        fetch('http://localhost:8080/dados')
+        .then(res => isOk(res))
+        .then(res => res.json())
+        .then((dados: NegociacaoParcial[]) => {
+            dados
+                .map(dado => new Negociacao(new Date(), dado.vezes, dado.montante))
+                .forEach(negociacao => this._negociacoes.adiciona(negociacao))
+            this._negociacoesView.update(this._negociacoes)
+        })
+        .catch(err => console.log(err.message))
+    }
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
